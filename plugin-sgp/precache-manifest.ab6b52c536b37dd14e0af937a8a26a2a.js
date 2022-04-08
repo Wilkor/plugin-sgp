6bdfdd4ba0b7850690e09f5add967d7f6c682d1b
Wilkor <wilkorguitarrista@msn.com>
2022-04-08 18:21:40 -0300

    Primeiro commit plugin sgp
    subindo a primeira versão do plugin

---
+++ plugin-sgp/precache-manifest.ab6b52c536b37dd14e0af937a8a26a2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142639ffb491979dd750bed2bf29d950",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5316976fb35e83d7f7ef",
+    "url": "./static/css/2.dc04a4c0.chunk.css"
+  },
+  {
+    "revision": "11cdf2f412b07b788698",
+    "url": "./static/css/main.ab44527d.chunk.css"
+  },
+  {
+    "revision": "5316976fb35e83d7f7ef",
+    "url": "./static/js/2.48c981e3.chunk.js"
+  },
+  {
+    "revision": "7895ddcdcff9b1aca87fb499ea0e4802",
+    "url": "./static/js/2.48c981e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11cdf2f412b07b788698",
+    "url": "./static/js/main.dc06e252.chunk.js"
+  },
+  {
+    "revision": "f36a3b9657e425b60302",
+    "url": "./static/js/runtime-main.637ebe85.js"
+  }
+]);
